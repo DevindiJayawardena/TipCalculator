97f427a9aa37f6fbecbd403dc58410ec97765fe2
Devindi Jayawardena <83491639+DevindiJayawardena@users.noreply.github.com>
2025-11-22 23:16:03 +0530

    Refactor ContentView.swift for better readability

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -1,68 +1,68 @@
-//
-//  ContentView.swift
-//  TipCalculator
-//
-//  Created by Devindi Jayawardena on 2022-11-13.
-//
 
-//import SwiftUI
-//
-//struct ContentView: View {
-//
-//    @State var total = ""
-//    @State var tipPercent = 15.0
-//
-//    var body: some View {
-//        VStack{
-//            HStack {
-//                Image(systemName: "dollarsign.circle.fill")
-//                    .imageScale(.medium)
-//                    .foregroundColor(.accentColor)
-//                    .font(.largeTitle)
-//                Text("Tip Calculator")
-//                    .font(.largeTitle)
-//                    .fontWeight(.bold)
-//                    .foregroundColor(Color.black)
-//            }
-//
-//            HStack {
-//                Text("")
-//            }
-//
-//            HStack {
-//                Text("Rs.")
-//                TextField("Amount", text: $total)
-//                //Text(total)
-//            }
-//
-//            HStack{
-//                Slider(
-//                    value: $tipPercent,
-//                    in: 1...30,
-//                    step: 1.0
-//                )
-//                Text("\(Int(tipPercent))")
-//                Text("%")
-//            }
-//
-//            if let totalNumber = Double(total){
-//                Text(verbatim: "Tip Amount : Rs. \(totalNumber * tipPercent / 100)")
-//            } else{
-//                Text("Please enter a numeric value")
-//            }
-//
-//        }
-//        .padding()
-//    }
-//
-//}
-//
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+ ContentView.swift
+ TipCalculator
+
+ Created by Devindi Jayawardena on 2022-11-13.
+
+
+import SwiftUI
+
+struct ContentView: View {
+
+   @State var total = ""
+   @State var tipPercent = 15.0
+
+   var body: some View {
+       VStack{
+           HStack {
+               Image(systemName: "dollarsign.circle.fill")
+                   .imageScale(.medium)
+                   .foregroundColor(.accentColor)
+                   .font(.largeTitle)
+               Text("Tip Calculator")
+                   .font(.largeTitle)
+                   .fontWeight(.bold)
+                   .foregroundColor(Color.black)
+           }
+
+           HStack {
+               Text("")
+           }
+
+           HStack {
+               Text("Rs.")
+               TextField("Amount", text: $total)
+               //Text(total)
+           }
+
+           HStack{
+               Slider(
+                   value: $tipPercent,
+                   in: 1...30,
+                   step: 1.0
+               )
+               Text("\(Int(tipPercent))")
+               Text("%")
+           }
+
+           if let totalNumber = Double(total){
+               Text(verbatim: "Tip Amount : Rs. \(totalNumber * tipPercent / 100)")
+           } else{
+               Text("Please enter a numeric value")
+           }
+
+       }
+       .padding()
+   }
+
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+   static var previews: some View {
+       ContentView()
+   }
+}
 
 
 
